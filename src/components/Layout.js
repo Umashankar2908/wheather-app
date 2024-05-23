90c2c1c6df9b9a334450801b584e93c1890c651e
Uma Shankar <umashankar12808@gmail.com>
2024-05-23 22:28:57 +0530

    initial commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,23 +6,38 @@ export const Layout = ({ children }) => {
   const [locale, setLocale] = useState("en");
   const [location, setLocation] = useState();
   const [loading, setLoading] = useState(false);
+  const [showEnableLocation, setShowEnableLocation] = useState();
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    } else {
-      console.log("Geolocation not supported");
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            setShowEnableLocation("do not show");
+            navigator.geolocation.getCurrentPosition(success, error);
+            //If granted then you can directly call your function here
+          } else if (result.state === "prompt") {
+            navigator.geolocation.getCurrentPosition(success, error);
+            setShowEnableLocation("show");
+          } else if (result.state === "denied") {
+            setShowEnableLocation("show");
+            //If denied then you have to show instructions to enable location
+          }
+        });
     }
 
     function success(position) {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
+      setShowEnableLocation("do not show");
       setLocation({ lat: latitude, lon: longitude });
       console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
     }
 
     function error() {
       setLocation({});
+      setShowEnableLocation("show");
       console.log("Unable to retrieve your location");
     }
   }, []);
@@ -31,7 +46,17 @@ export const Layout = ({ children }) => {
     <AppContext.Provider
       value={{ locale, setLocale, location, setLocation, loading, setLoading }}
     >
-      <Loader loading={loading}>{children}</Loader>
+      {showEnableLocation === "show" && (
+        <div className="h-[100vh] w-[100vw] flex-col flex z-[9999999] justify-center items-center bg-white">
+          <img src="access-icon.png" alt="" height={"40%"} width={"40%"} />
+          <div className="text-[40px] font-primary font-[700] pt-[40px]">
+            Please Allow Location access
+          </div>
+        </div>
+      )}
+      {showEnableLocation !== "show" && (
+        <Loader loading={loading}>{children}</Loader>
+      )}
     </AppContext.Provider>
   );
 };
